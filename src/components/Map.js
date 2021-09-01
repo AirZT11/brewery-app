7a8004b036697f9a13178ceadc7bec405e3a0fdd
airzt11 <airzonatea@gmail.com>
2021-09-01 17:35:37 -0400

    pick up from importing react google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+// import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css'
 import BreweryCard from './BreweryCard';
-import 'mapbox-gl/dist/mapbox-gl.css'
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+
+const libraries = ["places"]
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+}
+const center = {
+  lat: 104.9903,
+  lng: 39.7392
+}
 
 const Map = ({breweries}) => {
-  
-  const [viewport, setviewPort] = useState({
-    latitude: 39.6361637,
-    longitude: -105.321458,
-    width: '100vw',
-    height: '50vh',
-    zoom: 4
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries
   });
   
   // sets a selected brewery in state
-  const [selectedBrew, setSelectedBrew] = useState(null);
+  // const [selectedBrew, setSelectedBrew] = useState(null);
 
-  useEffect(() => {
-    const listener = e => {
-      if (e.key === "Escape") {
-        setSelectedBrew(null)
-      }
-    }
-    window.addEventListener('keydown', listener)
-  }, [])
+  // useEffect(() => {
+  //   const listener = e => {
+  //     if (e.key === "Escape") {
+  //       setSelectedBrew(null)
+  //     }
+  //   }
+  //   window.addEventListener('keydown', listener)
+  // }, [])
+
+  if (loadError) return "Error loading maps"
+  if (!isLoaded) return "Loading Map..."
+  
   return (
-    <div className='map'>
-      < ReactMapGL 
+    <div /*className='map'*/>
+      {/* < ReactMapGL 
         {...viewport} 
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle='mapbox://styles/airzt11/ckoyq05k2288l17nox4ns6ttl'
@@ -48,18 +59,21 @@ const Map = ({breweries}) => {
               <img src="/Lager2.svg" alt='beer'/>
             </button>
           </Marker>
-        ))}
+        ))} */}
 
-    {/* Displays brewery info on marker */}
-    {selectedBrew ? <Popup latitude={parseInt(selectedBrew.latitude)} longitude={parseInt(selectedBrew.longitude)}
-        closeButton={true}
-        closeOnClick={false}
-        onClose={() => setSelectedBrew(null)}
-        anchor="top" >
-        < BreweryCard brewery={selectedBrew}/>
-      </Popup> : null }
+        {/* Displays brewery info on marker */}
+        {/* {selectedBrew ? <Popup latitude={parseInt(selectedBrew.latitude)} longitude={parseInt(selectedBrew.longitude)}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={() => setSelectedBrew(null)}
+          anchor="top" >
+          < BreweryCard brewery={selectedBrew}/>
+        </Popup> : null }
       
-      </ReactMapGL>
+      </ReactMapGL> */}
+
+      <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} />
+
     </div>
   )
 }
