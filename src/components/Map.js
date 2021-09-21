b24aa4b62cb59a1c859d3c4e4dfd79e59c875e88
airzt11 <airzonatea@gmail.com>
2021-09-21 14:07:03 -0400

    Move GoogleSearch component into own component

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import { connect } from "react-redux";
 
 import Locate from "./Locate";
+import GoogleSearch from "./GoogleSearch";
 import BreweryCard from "./BreweryCard";
 import "../css/Map.css";
 import {
@@ -11,18 +12,6 @@ import {
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -66,7 +55,7 @@ const Map = ({ breweries, userLocation }) => {
 
   return (
     <div>
-      <Search userLocation={userLocation} panTo={panTo} />
+      {/* <GoogleSearch userLocation={userLocation} panTo={panTo} /> */}
       <Locate panTo={panTo} />
 
       <GoogleMap
@@ -117,61 +106,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Map);
-
-function Search({ userLocation, panTo }) {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      location: { lat: () => userLocation.lat, lng: () => userLocation.lng },
-      radius: 500 * 1000,
-    },
-  });
-
-  const handleInput = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
-
-    try {
-      const results = await getGeocode({ address });
-      const { lat, lng } = await getLatLng(results[0]);
-      panTo({ lat, lng });
-    } catch (error) {
-      console.log("😱 Error: ", error);
-    }
-  };
-
-  // DISPLAYS SEARCH BAR FOR GOOGLE
-  return (
-    <div className="search">
-      <Combobox
-        onSelect={async (address) => {
-          handleSelect(address);
-        }}
-      >
-        <ComboboxInput
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Enter in address"
-        />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ id, description }) => (
-                <ComboboxOption key={id} value={description} />
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    </div>
-  );
-}
